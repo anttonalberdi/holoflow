e4d81295043e811c792cdd95ce5fcfe12f311b39
nuriaher <nuria.albala@sund.ku.dk>
2020-06-18 11:13:56 +0200

    holoflow.py to qsub

--- holoflow.py
+++ holoflow.py
@@ -31,67 +31,65 @@ cores=args.threads
 def in_out_preprocessing(path,in_f):
     """Generate output names files from input.txt. Rename and move
     input files where snakemake expects to find them if necessary."""
-    # Create "00-RawData/" directory if not exists
+    # Define input directory and create it if not exists "00-InputData"
     in_dir = os.path.join(path,"PPR_00-InputData")
     if not os.path.exists(in_dir):
         os.makedirs(in_dir)
 
     with open(in_f,'r') as in_file:
-        # Paste desired output file names from input.txt
+        # Generate desired output file names from input.txt
         read = 0
         output_files=''
         final_temp_dir="PPR_04-MappedToHuman"
 
-        lines = in_file.readlines()
+        lines = in_file.readlines() # Read input.txt lines
         for file in lines:
 
             if not (file.startswith('#')):
-                file = file.strip('\n').split(' ')
+                file = file.strip('\n').split(' ') # Create a list of each line
 
-                read+=1
+                read+=1     # every sample will have two reads, keep the name of the file but change the read
+                # Add an output file based on input.txt info to a list for Snakemake command
                 output_files+=(path+"/"+final_temp_dir+"/"+file[0]+"_"+str(read)+".fastq ")
 
-                #Move files to new dir "00-InputData" and change file names for 1st column in input.txt
-                filename=file[2]
-                desired_filename='"'+in_dir+'/'+file[0]+'_'+str(read)+'.fastq"'
+                # Move files to new dir "00-InputData" and change file names for 1st column in input.txt
+                #   if the current input file names do not match the designed ones in input.txt
+                filename=file[2]       # current input file path and name
+                desired_filename='"'+in_dir+'/'+file[0]+'_'+str(read)+'.fastq"'  # desired input file path and name specified in input.txt
 
                 if not ((filename == desired_filename) and (os.path.exists(str(desired_filename)))):
-                    if filename.endswith('.gz'):
+                    if filename.endswith('.gz'):    # uncompress input file if necessary
                         uncompressCmd='gunzip -c '+filename+' > '+desired_filename+''
                         subprocess.check_call(uncompressCmd, shell=True)
-                    else:
+                    else:                           # else just move the input file to "00-InputData" with the new name
                         copyfilesCmd='cp '+filename+' '+desired_filename+''
                         subprocess.check_call(copyfilesCmd, shell=True)
 
 
                 if read == 2:
-                    read=0
-                    # Add stats output only once per sample
+                    read=0  # two read files for one sample finished, new sample
+
+                    # Add stats output file only once per sample
                     output_files+=(path+"/"+final_temp_dir+"/"+file[0]+".stats ")
 
         return output_files
 
 
 
-
-
 def run_preprocessing(in_f, path, config, cores):
-    """Create preprocessing.sh file and run snakemake on shell"""
+    """Run snakemake on shell"""
+    
     # Define output names
     out_files = in_out_preprocessing(path,in_f)
+    path_snkf = os.path.join(curr_dir,'workflows/preprocessing/Snakefile')
 
-    # Create preprocessing.sh for later job submission
-    with open('./workflows/preprocessing/preprocessing.sh','w+') as sh:
-        curr_dir = os.getcwd()
-        path_snkf = os.path.join(curr_dir,'workflows/preprocessing/Snakefile')
-        prep_snk = 'snakemake -s '+path_snkf+' '+out_files+' --configfile '+config+' --cores '+cores+''
-        sh.write(prep_snk)
+    # Run snakemake
+    prep_snk_Cmd = 'snakemake -s '+path_snkf+' '+out_files+' --configfile '+config+' --cores '+cores+''
+    subprocess.check_call(prep_snk_Cmd, shell=True)
+    print("Have a nice run!\n\t\tHOLOFOW Preprocessing starting")
 
 
-    # Submit snakemake job
-    preprocessingCmd = 'qsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e '+path+'/Holo-preprocessing.err -o '+path+'/Holo-preprocessing.out -l nodes=1:ppn=40,mem=180gb,walltime=5:00:00:00 -N Holoflow-preprocessing ./workflows/preprocessing/preprocessing.sh'
-    subprocess.check_call(preprocessingCmd, shell=True)
-    print("Preprocessing with Holoflow was successfully submited")
+
 
 
 
@@ -111,58 +109,58 @@ def in_out_metagenomics(path,in_f):
         output_files=''
         final_temp_dir="MIA_03-Binning"
 
-        lines = in_file.readlines()
+        lines = in_file.readlines() # Read input.txt lines
         for file in lines:
 
             if not (file.startswith('#')):
-                file = file.strip('\n').split(' ')
+                file = file.strip('\n').split(' ') # Create a list of each line
 
-                read+=1
+                read+=1 # every sample will have two reads, keep the name of the file but change the read
 
+                # Add an output file based on input.txt info to a list for Snakemake command
                 output_files+=(path+"/"+final_temp_dir+"/"+file[0]+"_dastool/"+file[0])
 
 
-                #Move files to input dir "PPR_04-MappedToHuman/" and change file names for column 1 in input.txt
-                filename=file[2]
-                desired_filename='"'+in_dir+'/'+file[0]+'_'+str(read)+'.fastq"'
+                # Move files to new dir "PPR_04-MappedToHuman/" and change file names for 1st column in input.txt
+                #   if the current input file names do not match the designed ones in input.txt
+                filename=file[2]       # current input file path and name
+                desired_filename='"'+in_dir+'/'+file[0]+'_'+str(read)+'.fastq"' # desired input file path and name specified in input.txt
 
                 if not ((filename == desired_filename) and (os.path.exists(str(desired_filename)))):
-                    if filename.endswith('.gz'):
+                    if filename.endswith('.gz'): # uncompress input file if necessary
                         uncompressCmd='gunzip -c '+filename+' > '+desired_filename+''
                         subprocess.check_call(uncompressCmd, shell=True)
 
-                    else:
+                    else:   # else just move the input file to "00-InputData" with the new name
                         copyfilesCmd='cp '+filename+' '+desired_filename+''
                         subprocess.check_call(copyfilesCmd, shell=True)
 
 
-                if read == 2:
+                if read == 2: # two read files for one sample finished, new sample
                     read=0
-                    # Add stats output only once per sample
+                    # Add stats output file only once per sample
                     output_files+=(path+"/"+final_temp_dir+"/"+file[0]+".stats ")
 
         return output_files
 
 
 
+
 def run_metagenomics(in_f, path, config, cores):
-    """Create metagenomics.sh file and run snakemake on shell"""
+    """Run snakemake on shell"""
 
     # Define output names
     out_files = in_out_metagenomics(path,in_f)
+    path_snkf = os.path.join(curr_dir,'workflows/metagenomics/individual_assembly/Snakefile')
+
+    # Run snakemake
+    mtg_snk_Cmd = 'snakemake -s '+path_snkf+' '+out_files+' --configfile '+config+' --cores '+cores+''
+    subprocess.check_call(mtg_snk_Cmd, shell=True)
+
+    print("Have a nice run!\n\t\tHOLOFOW Metagenomics starting")
 
 
-    # # Create preprocessing.sh for later job submission
-    with open('./workflows/metagenomics/individual_assembly/metagenomics.sh','w+') as sh:
-        curr_dir = os.getcwd()
-        path_snkf = os.path.join(curr_dir,'workflows/metagenomics/individual_assembly/Snakefile')
-        prep_snk = 'snakemake -s '+path_snkf+' '+out_files+' --configfile '+config+' --cores '+cores+''
-        sh.write(prep_snk)
 
-
-    metagenomicsCmd = 'qsub -V -A ku-cbd -W group_list=ku-cbd -d `pwd` -e '+path+'/Holo-metagenomics.err -o '+path+'/Holo-metagenomics.out -l nodes=1:ppn=40,mem=180gb,walltime=5:00:00:00 -N Holoflow-metagenomics ./workflows/metagenomics/individual_assembly/metagenomics.sh'
-    subprocess.check_call(metagenomicsCmd, shell=True)
-    print("Great! Have a nice run!\n\t\tHOLOFOW Metagenomics starting")
 
 
 
@@ -189,6 +187,10 @@ subprocess.check_call(load_modulesCmd, shell=True)
 #### Workflows
 ###########################
 
+# 0    # Prepare genomes workflow
+
+
+
 # 1    # Preprocessing workflow
 if workflow == "preprocessing":
     run_preprocessing(in_f, path, config, cores)
@@ -196,29 +198,8 @@ if workflow == "preprocessing":
 
 # 2    # Metagenomics workflow
 
-if workflow == "metagenomics":
-
-    prepdata = input("Is your data preprocessed into fastq files? [y/n]")
-
-    if prepdata == 'n':
-        prepdata2 = input("Would you like to process it before running holoflow/metagenomics with holoflow/preprocessing? [y/n]")
-
-        if prepdata2 == 'n':
-            print("You should come back when your data is preprocessed. See you soon :)")
-
-        # if prepdata2 == 'y':    # It would be much easier to concatenate Snakefiles and new functions - DO IT
-        #     prep_in_f = input("Could you please state the path for the preprocessing input file? - No quoting needed\n")
-        #     prep_config = input("Could you please state the path for the preprocessing config file? - No quoting needed\n")
-        #     run_preprocessing(prep_in_f, path, prep_config, cores)
-        #
-        #     prep_out_dir = os.path.join(path,"04-MappedToHuman")
-        #     if os.path.exists(prep_out_dir):
-        #         run_metagenomics(in_f, path, config, cores)
-
-    if prepdata == 'y':
-        run_metagenomics(in_f, path, config, cores)
-
-
+if workflow == "metagenomics": # DATA HAS TO BE PREPROCESSED!
+    run_metagenomics(in_f, path, config, cores)
 
 
 # 3    # Genomics workflow
