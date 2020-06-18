ff972088e1343471cd2a240ab4d79471eadf69e0
nuriaher <nuria.albala@sund.ku.dk>
2020-06-18 15:37:57 +0200

    prep/mapping upd

--- bin/holo-map_ref_split.py
+++ bin/holo-map_ref_split.py
@@ -10,6 +10,8 @@ parser.add_argument('-ibam', help="all bam file", dest="all_bam", required=True)
 parser.add_argument('-1', help="path1", dest="read1", required=True)
 parser.add_argument('-2', help="path2", dest="read2", required=True)
 parser.add_argument('-obam', help="bam file", dest="bam", required=True)
+parser.add_argument('-si', help="stats input file", dest="in_stats", required=True)
+parser.add_argument('-so', help="stats output file", dest="out_stats", required=True)
 args = parser.parse_args()
 
 all_bam=args.all_bam
@@ -27,3 +29,25 @@ subprocess.check_call(refbam2Cmd, shell=True)
 
 rmAllbamCmd = 'rm '+all_bam+''
 subprocess.check_call(rmAllbamCmd, shell=True)
+
+
+
+    # Get stats after duplicate removal
+mvstatsCmd= 'mv '+in_stats+' '+out_stats+''
+subprocess.check_call(mvstatsCmd, shell=True)
+
+
+reads = 0
+bases = 0
+with open(str(read1), 'rb') as read:
+    for id in read:
+        seq = next(read)
+        reads += 1
+        bases += len(seq.strip())*2
+        next(read)
+        next(read)
+
+#Print stats to statsfile
+statsfile=open(str(out_stats),"a+")
+statsfile.write("Reads after mapping to reference genome \t{0} ({1} bases)\r\n".format(reads,bases))
+statsfile.close()
