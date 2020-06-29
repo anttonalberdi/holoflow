b1048187dd1e58e53e4a57235e7f0b452b4f7923
nuriaher <nuria.albala@sund.ku.dk>
2020-06-29 14:42:00 +0200

    preparegenomes upd

--- bin/holo-check_compress.py
+++ bin/holo-check_compress.py
@@ -9,22 +9,22 @@ import os
 parser = argparse.ArgumentParser(description='Runs holoflow pipeline.')
 parser.add_argument('-db', help="data base file", dest="db", required=True)
 parser.add_argument('-idx_db', help="indexed data base file", dest="idx_db", required=True)
+parser.add_argument('-db_dir', help="data base directory", dest="db_dir", required=True)
 parser.add_argument('-check', help="file OK", dest="check", required=True)
 args = parser.parse_args()
 
 
 db=args.db
 idx_db=args.idx_db
+db_dir=args.db_dir
 check=args.check
 
 
 
 # Run
 if (os.path.exists(str(idx_db)) and os.path.exists(str(db))):
-    file = os.path.dirname(sys.argv[0])
-    curr_dir = os.path.abspath(file)
 
-    compressCmd=('tar -zcvf '+db+'.tar.gz '+curr_dir+'')
+    compressCmd=('tar -zcvf '+db+'.tar.gz '+db_dir+'')
     subprocess.check_call(compressCmd, shell=True)
 
     with open(str(check),'w') as check_file:
