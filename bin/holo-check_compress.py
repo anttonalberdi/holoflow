6969b2086b6e984c6b9184645db22018eb75b94a
nuriaher <nuria.albala@sund.ku.dk>
2020-06-29 15:51:11 +0200

    upd

--- bin/holo-check_compress.py
+++ bin/holo-check_compress.py
@@ -3,6 +3,7 @@
 import subprocess
 import argparse
 import sys
+import time
 import os
 
 #Argument parsing
@@ -10,6 +11,7 @@ parser = argparse.ArgumentParser(description='Runs holoflow pipeline.')
 parser.add_argument('-db', help="data base file", dest="db", required=True)
 parser.add_argument('-idx_db', help="indexed data base file", dest="idx_db", required=True)
 parser.add_argument('-db_dir', help="data base directory", dest="db_dir", required=True)
+parser.add_argument('-log', help="pipeline log file", dest="log", required=True)
 parser.add_argument('-check', help="file OK", dest="check", required=True)
 args = parser.parse_args()
 
@@ -17,15 +19,30 @@ args = parser.parse_args()
 db=args.db
 idx_db=args.idx_db
 db_dir=args.db_dir
+log=args.log
 check=args.check
 
 
 
 # Run
-if (os.path.exists(str(idx_db)) and os.path.exists(str(db))):
+# Write to log
+current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+with open(str(log),'a+') as log:
+    log.write('\t\t'+current_time+'\tCompressing data base and index files step\n\n')
+
+
+
+
+if (os.path.exists(str(idx_db)) and os.path.exists(str(db))) and (not os.path.exists(str(check))):
 
     compressCmd=('tar -zcvf '+db+'.tar.gz '+db_dir+'')
     subprocess.check_call(compressCmd, shell=True)
 
     with open(str(check),'w') as check_file:
         check_file.write('All reference genomes have been merged and indexed successfully.')
+
+
+# Write to log
+current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+with open(str(log),'a+') as log:
+    log.write('\t\t'+current_time+'\tHoloflow has completed the preparation of the reference genomes.\n\n')
--- bin/holo-db_index.py
+++ bin/holo-db_index.py
@@ -2,41 +2,58 @@
 
 import subprocess
 import argparse
+import time
 import os
 
 #Argument parsing
 parser = argparse.ArgumentParser(description='Runs holoflow pipeline.')
 parser.add_argument('-db', help="data base file", dest="db", required=True)
 parser.add_argument('-idx_bwa', help="index data base file bwa", dest="idx_bwa", required=True)
+parser.add_argument('-log', help="pipeline log file", dest="log", required=True)
 parser.add_argument('-idx_smt', help="index data base file samtools", dest="idx_smt", required=True)
 args = parser.parse_args()
 
 
 db=args.db
+log=args.log
 idx_bwa=args.idx_bwa
 idx_smt=args.idx_smt
 
 
 # Run
-if (os.path.exists(str(idx_bwa)) and os.path.exists(str(idx_smt))):
+
+# Write to log
+current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+with open(str(log),'w+') as log:
+    log.write('\tHOLOFLOW\tPREPARE GENOMES\n\t\t'+current_time+'\tData Base indexing step\n')
+    log.write('The data base needs to be indexed with BWA and SAMTOOLS so it can be mapped during preprocessing.\n\n')
+
+
+
+# first decompress db if necessary
+if str(db).endswith(".gz"):
+    decompressCmd=('gunzip '+db+'')
+    subprocess.check_call(decompressCmd, shell=True)
+    decomp_db= db.replace('.gz','')
+
+else:
+    decomp_db = db
+
+# Index
+
+if os.path.exists(str(idx_bwa)):
     pass
 
 else:
-    # first decompress db
-    if str(db).endswith(".gz"):
-        decompressCmd=('gunzip '+db+'')
-        subprocess.check_call(decompressCmd, shell=True)
-        decomp_db= db.replace('.gz','')
+    idxbwaCmd='module load bwa/0.7.15 && bwa index '+decomp_db+''
+    subprocess.check_call(idxbwaCmd, shell=True)
 
-        # index
-        idxsamCmd='module load tools samtools/1.9 && samtools faidx '+decomp_db+''
-        idxbwaCmd='module load bwa/0.7.15 && bwa index '+decomp_db+''
-        subprocess.check_call(idxbwaCmd, shell=True)
-        subprocess.check_call(idxsamCmd, shell=True)
 
-    else:
-        # index
-        idxsamCmd='module load tools samtools/1.9 && samtools faidx '+db+''
-        idxbwaCmd='module load bwa/0.7.15 && bwa index '+db+''
-        subprocess.check_call(idxbwaCmd, shell=True)
-        subprocess.check_call(idxsamCmd, shell=True)
+
+if os.path.exists(str(idx_smt)):
+    pass
+
+else:
+    # index
+    idxsamCmd='module load tools samtools/1.9 && samtools faidx '+decomp_db+''
+    subprocess.check_call(idxsamCmd, shell=True)
