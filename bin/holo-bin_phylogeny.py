cde2252dd83a7d7038cda3be510b208b53add9ac
nuriaher <nuria.albala@sund.ku.dk>
2020-11-12 09:41:54 +0100

    drp upd

--- bin/holo-bin_phylogeny.py
+++ bin/holo-bin_phylogeny.py
@@ -9,7 +9,7 @@ import time
 
 #Argument parsing
 parser = argparse.ArgumentParser(description='Runs holoflow pipeline.')
-parser.add_argument('-gen_dir', help="prokka genes directory", dest="gene_dir", required=True)
+parser.add_argument('-genome_dir', help="genomes directory", dest="gen_dir", required=True)
 parser.add_argument('-out_dir', help="main output directory", dest="out_dir", required=True)
 parser.add_argument('-ID', help="ID", dest="ID", required=True)
 parser.add_argument('-log', help="pipeline log file", dest="log", required=True)
@@ -17,7 +17,7 @@ parser.add_argument('-t', help="threads", dest="threads", required=True)
 args = parser.parse_args()
 
 
-gene_dir=args.gene_dir
+gen_dir=args.gen_dir
 out_dir=args.out_dir
 ID=args.ID
 log=args.log
@@ -32,6 +32,11 @@ if not (os.path.exists(str(out_dir))):
     current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
     with open(str(log),'a+') as logi:
         logi.write('\t\t'+current_time+'\tTaxonomic Classification step - '+ID+'\n')
-        logi.write('GTDB-Tk is assigning objective taxonomic classifications to baterial genomes based on the Genome Database Taxonomy GTDB.\n\n')
+        logi.write('GTDB-Tk is assigning objective taxonomic classifications to baterial genomes based on the Genome Database Taxonomy GTDB.\nThe taxonomic classification of each genome can be found in the .summary.tsv file.\n\n')
 
-        
+
+    #if args.reference: # Classify genomes by placement in GTDB reference tree
+    gtdbtkCmd='module load tools anaconda3/4.4.0 prodigal/2.6.3 hmmer/3.2.1 anaconda2/4.4.0 pplacer/1.1.alpha19 fastani/1.1 && gtdbtk classify_wf --genome_dir '+gen_dir+' --extension "fa" --out_dir '+out_dir+' --cpus '+threads+''
+    subprocess.Popen(gtdbtkCmd,shell=True).wait()
+
+    #if args.denovo: # Infer de novo tree and decorate with GTDB taxonomy # Requires OUTGROUP
