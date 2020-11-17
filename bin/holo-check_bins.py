5a480c5f2862ed4c7d1e14c246e816fe66272f37
nuriaher <nuria.albala@sund.ku.dk>
2020-11-17 11:44:50 +0100

    upd

--- bin/holo-check_bins.py
+++ bin/holo-check_bins.py
@@ -9,19 +9,15 @@ import sys
 
 #Argument parsing
 parser = argparse.ArgumentParser(description='Runs holoflow pipeline.')
-parser.add_argument('-bt_mtb', help="metabat bin table", dest="bt_mtb", required=True)
-parser.add_argument('-bt_mxb', help="maxbin bin table", dest="bt_mxb", required=True)
-parser.add_argument('-mtb', help="metabat bin dir", dest="mtb", required=True)
-parser.add_argument('-mxb', help="maxbin bin dir", dest="mxb", required=True)
+parser.add_argument('-binning_dir', help="binning directory", dest="binning_dir", required=True)
+parser.add_argument('-check_file', help="empty check file", dest="check_file", required=True)
 parser.add_argument('-ID', help="ID", dest="ID", required=True)
 parser.add_argument('-log', help="pipeline log file", dest="log", required=True)
 args = parser.parse_args()
 
 
-bt_mtb=args.bt_mtb
-bt_mxb=args.bt_mxb
-mtb=args.mtb
-mxb=args.mxb
+binning_dir=args.binning_dir
+check_file=args.check_file
 ID=args.ID
 log=args.log
 
@@ -29,8 +25,14 @@ log=args.log
 #################### WRITE TO LOG ##########################
 ##############################################
 
-    # If only one of the binners produced bins:
+mtb=str(os.path.join(binning_dir,ID+'_metabat'))
+bt_mtb=str(binning_dir+'/'+ID+'.bins_metabat.txt')
+mxb=str(os.path.join(binning_dir,ID+'_maxbin'))
+bt_mxb=str(binning_dir+'/'+ID+'.bins_maxbin.txt')
 
+
+
+    # If only one of the binners produced bins:
 bt_todupl=''
 bp_todupl=''
 bt_e=''
@@ -38,7 +40,7 @@ bp_e=''
 dupl_binner=''
 empty_binner=''
 
-if not (os.path.exists(bt_mtb)):
+if not (os.path.isfile(bt_mtb)):
     bt_todupl=bt_mxb
     bp_todupl=mxb
     dupl_binner='mxb'
@@ -47,7 +49,10 @@ if not (os.path.exists(bt_mtb)):
     bp_e=mtb
     empty_binner='mtb'
 
-if not (os.path.exists(bt_mxb)):
+    if os.path.exists(bp_e):
+        os.rmdir(bp_e)
+
+if not (os.path.isfile(bt_mxb)):
     bt_todupl=bt_mtb
     bp_todupl=mtb
     dupl_binner='mtb'
@@ -56,20 +61,20 @@ if not (os.path.exists(bt_mxb)):
     bp_e=mxb
     empty_binner='mxb'
 
-if (os.path.exists(bt_mxb) and os.path.exists(bt_mtb)):
+    if os.path.exists(bp_e):
+        os.rmdir(bp_e)
+
+else:
+    os.mknod(str(check_file))
     sys.exit()
 
 
 # Duplicate the existing bins and bin table and rename duplicates
-if os.path.exists(bp_e):
-    os.rmdir(bp_e)
+mvCmd='cp -r '+bp_todupl+' '+bp_e+' && for f in '+bp_e+'/*'+str(dupl_binner)+'* ; do mv "$f" "$(echo "$f" | sed s/'+str(dupl_binner)+'/dup_'+str(empty_binner)+'/)"; done'
+subprocess.Popen(mvCmd,shell=True).wait()
 
-    mvCmd='cp -r '+bp_todupl+' '+bp_e+' && cp '+bt_todupl+' '+bt_e+' && grep '+str(dupl_binner)+' '+bp_e+'/* | for f in ; do mv "$f" "$(echo "$f" | sed s/'+str(dupl_binner)+'/'+str(empty_binner)+'/)"; done && grep '+str(dupl_binner)+' '+bt_e+' | sed s/'+str(dupl_binner)+'/'+str(empty_binner)+'/'
-    subprocess.check_call(mvCmd,shell=True)
+cpCmd='cp '+bt_todupl+' '+bt_e+'.tmp && grep '+str(dupl_binner)+' '+bt_e+'.tmp | sed s/'+str(dupl_binner)+'/dup_'+str(empty_binner)+'/ > '+bt_e+' && rm '+bt_e+'.tmp'
+subprocess.Popen(cpCmd,shell=True).wait()
 
-else:
-    pass
-
-
-
-    
+emptyCmd='touch '+check_file+''
+subprocess.Popen(emptyCmd,shell=True).wait()
