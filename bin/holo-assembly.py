83069b94183ba9b8e337f039dbcca4e144bd2396
nuriaher <nuria.albala@sund.ku.dk>
2020-12-15 09:13:46 +0100

    upd

--- bin/holo-assembly.py
+++ bin/holo-assembly.py
@@ -48,7 +48,7 @@ with open(str(log),'a+') as log:
     log.write('The .fastq files coming from Holoflow Preprocessing, are those which could not be mapped to a \nreference genome. These contain the metagenomic reads; as no reference genome exists to them,\n they have to be assembled de novo. This is done by '+args.assembler+' here, which sorts the reads together into\ncontigs or scaffolds giving out one only assembly fasta file.\n\n')
 
 
-if not (os.path.exists(str(empty_o)) or os.path.exists(str(temp_a)) or os.path.exists(str(out))):
+if not (os.path.exists(str(empty_o)) or os.path.exists(str(temp_a))):
 
     emptytouchCmd='touch '+empty_o+''
     subprocess.check_call(emptytouchCmd, shell=True)
