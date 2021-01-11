ef33bdaf1944a8af12b65ca60e2b3fdfc572de97
nuriaher <nuria.albala@sund.ku.dk>
2021-01-11 12:17:37 +0100

    upd

--- genomics.py
+++ genomics.py
@@ -9,10 +9,10 @@ import sys
 parser = argparse.ArgumentParser(description='Runs holoflow pipeline.')
 parser.add_argument('-f', help="input.txt file", dest="input_txt", required=True)
 parser.add_argument('-d', help="temp files directory path", dest="work_dir", required=True)
-parser.add_argument('-c', help="config file", dest="config_file", required=False)
-parser.add_argument('-k', help="keep tmp directories", dest="keep", action='store_true')
 parser.add_argument('-g', help="reference genome path", dest="ref", required=True)
 parser.add_argument('-vc', help="variant caller: 1 {bcftools/samtools}, 2 {GATK}, 3 {ANGSD}", dest="var_c", required=True)
+parser.add_argument('-c', help="config file", dest="config_file", required=False)
+parser.add_argument('-k', help="keep tmp directories", dest="keep", action='store_true')
 parser.add_argument('-l', help="pipeline log file", dest="log", required=False)
 parser.add_argument('-t', help="threads", dest="threads", required=True)
 args = parser.parse_args()
@@ -29,12 +29,12 @@ curr_dir = os.path.abspath(file)
 
 
 if not (args.config_file):
-    config = os.path.join(os.path.abspath(curr_dir),"workflows/genomics/variant_calling/config.yaml")
+    config = os.path.join(os.path.abspath(curr_dir),"workflows/genomics/config.yaml")
 else:
     config=args.config_file
 
 if not (args.log):
-    log = os.path.join(path,"Holoflow_variant_calling.log")
+    log = os.path.join(path,"Holoflow_genomics.log")
 else:
     log=args.log
 
@@ -75,9 +75,9 @@ with open(str(config), 'w') as config_file:
 
 
     ###########################
-    ######  VARIANT CALLING FUNCTIONS
+    ######  genomics FUNCTIONS
 
-def in_out_variant_calling(path,in_f):
+def in_out_genomics(path,in_f):
     """Generate output names files from input.txt. Rename and move
     input files where snakemake expects to find them if necessary."""
     # Define input directory and create it if not exists "00-InputData"
@@ -107,52 +107,51 @@ def in_out_variant_calling(path,in_f):
                 # Define output files based on input.txt
                 output_files+=path+'/'+final_temp_dir+'/'+group+'/per_chr '
 
-
                 # Define input dir
                 in1=in_dir+'/'+group+''
 
                 # Check if input files already in desired dir
                 if os.path.exists(in1):
                     pass
-                else:
+                else: ############################################################################# CREATE LINK 
                     mvbamsCmd = 'cd '+in_bam_path+' && cp *.bam '+in1+'' ############################################################################################################## PROBABLY NOT THE BEST IDEA TO COPY ALL GENOMIC BAMS... ALTERNATIVE!
                     subprocess.Popen(mvbamsCmd, shell=True).wait()
 
-                # Append final bam input path to config for Snakefile run
-                # yaml = ruamel.yaml.YAML()
-                # yaml.explicit_start = True
-                # with open(str(config), 'r') as config_file:
-                #     data = yaml.load(config_file)
-                #
-                # with open(str(config), 'w') as config_file:
-                #     data['BAMs_path'] = str(in_bam_path).strip() ############################################################################################################## INSTEAD OF NEW COPIED PATH, USE THE GIVEN INPUT PATH TO SEND IT TO SNAKEMAKE
-                #     dump = yaml.dump(data, config_file)
+                # Append chromosome list path to config
+                chromosome_list = line[2]
+                yaml = ruamel.yaml.YAML()
+                yaml.explicit_start = True
+                with open(str(config), 'r') as config_file:
+                    data = yaml.load(config_file)
+                with open(str(config), 'w') as config_file:
+                    data['chr_list'] = str(chromosome_list)
+                    dump = yaml.dump(data, config_file)
 
         return output_files
 
 
 
-def run_variant_calling(in_f, path, config, cores):
+def run_genomics(in_f, path, config, cores):
     """Run snakemake on shell, wait for it to finish.
     Given flag, decide whether keep only last directory."""
 
     # Define output names
-    out_files = in_out_variant_calling(path,in_f)
+    out_files = in_out_genomics(path,in_f)
 
     curr_dir = os.path.dirname(sys.argv[0])
     holopath = os.path.abspath(curr_dir)
-    path_snkf = os.path.join(holopath,'workflows/genomics/variant_calling/Snakefile')
+    path_snkf = os.path.join(holopath,'workflows/genomics/Snakefile')
 
     # Run snakemake
     log_file = open(str(log),'w+')
-    log_file.write("Have a nice run!\n\t\tHOLOFOW Variant Calling starting")
+    log_file.write("Have a nice run!\n\t\tHOLOFOW Genomics starting")
     log_file.close()
 
-    variant_calling_snk_Cmd = 'module load tools anaconda3/4.4.0 && snakemake -s '+path_snkf+' -k '+out_files+' --configfile '+config+' --cores '+cores+''
-    subprocess.Popen(variant_calling_snk_Cmd, shell=True).wait()
+    genomics_snk_Cmd = 'module load tools anaconda3/4.4.0 && snakemake -s '+path_snkf+' -k '+out_files+' --configfile '+config+' --cores '+cores+''
+    subprocess.Popen(genomics_snk_Cmd, shell=True).wait()
 
     log_file = open(str(log),'a+')
-    log_file.write("\n\t\tHOLOFOW Variant Calling has finished :)")
+    log_file.write("\n\t\tHOLOFOW Genomics has finished :)")
     log_file.close()
 
     # Keep temp dirs / remove all
@@ -181,4 +180,4 @@ def run_variant_calling(in_f, path, config, cores):
 
 
 # 1    # Final Stats workflow
-run_variant_calling(in_f, path, config, cores)
+run_genomics(in_f, path, config, cores)
