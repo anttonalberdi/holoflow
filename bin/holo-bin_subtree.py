6c558162f50c44149c4f96eca90c3e2931d867d1
nuriaher <nuria.albala@sund.ku.dk>
2021-01-25 11:44:48 +0100

    upd

--- bin/holo-bin_subtree.py
+++ bin/holo-bin_subtree.py
@@ -28,62 +28,62 @@ log=args.log
 
 
 # Run
-#if not (os.path.isfile(bac_o)):
+if not (os.path.isfile(bac_o)):
 
-# Define in and out tree paths
-in_paths = sorted(glob.glob(tree_dir+'/*.tree'))
-out_paths = [ar_o,bac_o]
+    # Define in and out tree paths
+    in_paths = sorted(glob.glob(tree_dir+'/*.tree'))
+    out_paths = [ar_o,bac_o]
 
-# In case bins come from individually assembled samples: get all sample IDs in group
-# If bins come from coassembly, only one ID will be in the list
-bin_names = [os.path.basename(x) for x in glob.glob(bin_dir+'/*.fa')]
-ID_list = list()
-for ID in bin_names:
-    ID = ID.split('.')[0]   # Assume user won't use '.' in sample ID :()
-    if not ID in ID_list:
-        ID_list.append(ID)
-
-
-##### Subtract group's tree tips - omit gtdbtk's entries
-for i in range(len(in_paths)):
-    tree_path = in_paths[i]
-    out_tree_path = out_paths[i]
-    tree_data = str()
-    sample_tips = list()
-
-    # Read in tree
-    with open(tree_path,'r+') as tree:
-        for line in tree.readlines():
-            tree_data+=line
-
-    # Pattern search for user's bins
-    for ID in ID_list:
-        # Find between 1 and unlimited case insensitive letters (ID), this can include numbers or not.
-        # After that a . followed by three lower-case letters (mtb,cct,mxb) followed by 1,2,3 or 4 numbers (binner bin number)
-        # followed by ".fa"
-        match = re.findall(str(ID)+'[a-zA-Z]+[0-9]*\.{1}[a-z]{3}[0-9]{1}|[a-zA-Z]+[0-9]*\.{1}[a-z]{3}[0-9]{2}|[a-zA-Z]+[0-9]*\.{1}[a-z]{3}[0-9]{3}|[a-zA-Z]+[0-9]*\.{1}[a-z]{3}[0-9]{4}',tree_data)
-        if match:
-            sample_tips = sample_tips + match
+    # In case bins come from individually assembled samples: get all sample IDs in group
+    # If bins come from coassembly, only one ID will be in the list
+    bin_names = [os.path.basename(x) for x in glob.glob(bin_dir+'/*.fa')]
+    ID_list = list()
+    for ID in bin_names:
+        ID = ID.split('.')[0]   # Assume user won't use '.' in sample ID :()
+        if not ID in ID_list:
+            ID_list.append(ID)
 
 
-    # Re-assure pattern search (Sometimes some IDs include others, such as: sample ID LS includes sample ID S...)
-    # Check if tip (pattern matched bin base-name), exists in bin dir
-    final_tips = list ()
-    for tip in set(sample_tips):
-        #print(tip)
-        if (tip+'.fa' in bin_names):
-            final_tips.append(tip)
-        elif (tip+'_sub.fa' in bin_names):
-            final_tips.append(tip+'_sub')
-    final_tips = (',').join('"{0}"'.format(tip) for tip in final_tips)
+    ##### Subtract group's tree tips - omit gtdbtk's entries
+    for i in range(len(in_paths)):
+        tree_path = in_paths[i]
+        out_tree_path = out_paths[i]
+        tree_data = str()
+        sample_tips = list()
+
+        # Read in tree
+        with open(tree_path,'r+') as tree:
+            for line in tree.readlines():
+                tree_data+=line
+
+        # Pattern search for user's bins
+        for ID in ID_list:
+            # Find between 1 and unlimited case insensitive letters (ID), this can include numbers or not.
+            # After that a . followed by three lower-case letters (mtb,cct,mxb) followed by 1,2,3 or 4 numbers (binner bin number)
+            # followed by ".fa"
+            match = re.findall(str(ID)+'[a-zA-Z]+[0-9]*\.{1}[a-z]{3}[0-9]{1}|[a-zA-Z]+[0-9]*\.{1}[a-z]{3}[0-9]{2}|[a-zA-Z]+[0-9]*\.{1}[a-z]{3}[0-9]{3}|[a-zA-Z]+[0-9]*\.{1}[a-z]{3}[0-9]{4}',tree_data)
+            if match:
+                sample_tips = sample_tips + match
+
+
+        # Re-assure pattern search (Sometimes some IDs include others, such as: sample ID LS includes sample ID S...)
+        # Check if tip (pattern matched bin base-name), exists in bin dir
+        final_tips = list ()
+        for tip in set(sample_tips):
+            #print(tip)
+            if (tip+'.fa' in bin_names):
+                final_tips.append(tip)
+            elif (tip+'_sub.fa' in bin_names):
+                final_tips.append(tip+'_sub')
+        final_tips = (',').join('"{0}"'.format(tip) for tip in final_tips)
 
 
 
-    # Call Rscript to generate sub-trees
-    file = os.path.dirname(sys.argv[0])
-    curr_dir = os.path.abspath(file)
+        # Call Rscript to generate sub-trees
+        file = os.path.dirname(sys.argv[0])
+        curr_dir = os.path.abspath(file)
 
 
-    if final_tips:
-        subtreeCmd='module load tools gcc/5.4.0 intel/compiler/64/2018_update2 R/3.5.3-ICC-MKL && Rscript '+curr_dir+'/holo-bin_subtree.R --tips '+final_tips+' -in_tree '+tree_path+' -out_tree '+out_tree_path+''
-        subprocess.Popen(subtreeCmd,shell=True).wait()
+        if final_tips:
+            subtreeCmd='module load tools gcc/5.4.0 intel/compiler/64/2018_update2 R/3.5.3-ICC-MKL && Rscript '+curr_dir+'/holo-bin_subtree.R --tips '+final_tips+' -in_tree '+tree_path+' -out_tree '+out_tree_path+''
+            subprocess.Popen(subtreeCmd,shell=True).wait()
