7c48ecc099b7da3d09a414353d44eed6c905cb71
nuriaher <nuria.albala@sund.ku.dk>
2021-02-15 10:54:44 +0100

    upd

--- bin/holo-assembly.py
+++ bin/holo-assembly.py
@@ -54,17 +54,28 @@ if not (os.path.exists(str(empty_o)) or os.path.exists(str(temp_a)) or os.path.e
     subprocess.check_call(emptytouchCmd, shell=True)
 
 
-    if (args.assembler == "megahit") or (args.coassembly):
+    if (args.assembler == "megahit")
 
-        with open(read1,'r') as f1, open(read2,'r') as f2:
-            read1_paths = f1.readline()
-            read2_paths = f2.readline()
+        if (args.coassembly):
 
-        megahitCmd = 'module load tools megahit/1.2.9 && megahit -1 '+read1_paths+' -2 '+read2_paths+' -t '+threads+' --k-list '+k_megahit+' -o '+out+''
-        subprocess.check_call(megahitCmd, shell=True)
+            with open(read1,'r') as f1, open(read2,'r') as f2:
+                read1_paths = f1.readline()
+                read2_paths = f2.readline()
 
-        mv_megahitCmd = 'mv '+out+'/final.contigs.fa '+out+'/temp_assembly.fa'
-        subprocess.check_call(mv_megahitCmd, shell=True)
+            megahitCmd = 'module load tools megahit/1.2.9 && megahit -1 '+read1_paths+' -2 '+read2_paths+' -t '+threads+' --k-list '+k_megahit+' -o '+out+''
+            subprocess.check_call(megahitCmd, shell=True)
+
+            mv_megahitCmd = 'mv '+out+'/final.contigs.fa '+out+'/temp_assembly.fa'
+            subprocess.check_call(mv_megahitCmd, shell=True)
+
+        else:
+
+            megahitCmd = 'module load tools megahit/1.2.9 && megahit -1 '+read1+' -2 '+read2+' -t '+threads+' --k-list '+k_megahit+' -o '+out+''
+            subprocess.check_call(megahitCmd, shell=True)
+
+            mv_megahitCmd = 'mv '+out+'/final.contigs.fa '+out+'/temp_assembly.fa'
+            subprocess.check_call(mv_megahitCmd, shell=True)
+
 
 
     if args.assembler == "spades":
