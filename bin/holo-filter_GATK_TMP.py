2b8528e64e4eb56f5b9bddb57127a52a69ac43e8
nuriaher <nuria.albala@sund.ku.dk>
2021-02-26 12:12:33 +0100

    upd

---
+++ bin/holo-filter_GATK_TMP.py
@@ -0,0 +1,64 @@
+## 26.02.21 - Holoflow 0.1
+import subprocess
+import argparse
+import os
+import glob
+import time
+
+
+#Argument parsing
+parser = argparse.ArgumentParser(description='Runs holoflow pipeline.')
+parser.add_argument('-var_dir', help="variant files directory", dest="var_dir", required=True)
+parser.add_argument('-out_dir', help="main output directory", dest="out_dir", required=True)
+parser.add_argument('-chr_list', help="chromosome list file path", dest="chr_list", required=True)
+parser.add_argument('-ID', help="ID", dest="ID", required=True)
+parser.add_argument('-log', help="pipeline log file", dest="log", required=True)
+parser.add_argument('-t', help="threads", dest="threads", required=True)
+parser.add_argument('-QUAL', help="QUAL", dest="QUAL", required=True)
+parser.add_argument('-QD', help="QD", dest="QD", required=True)
+parser.add_argument('-FS', help="FS", dest="FS", required=True)
+args = parser.parse_args()
+
+
+var_dir=args.var_dir
+out_dir=args.out_dir
+chr_list=args.chr_list
+ID=args.ID
+log=args.log
+threads=args.threads
+QUAL=args.QUAL
+QD=args.QD
+FS=args.FS
+
+## Run
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+    # Write to log
+    current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+    with open(str(log),'a+') as logi:
+        logi.write('\t\t'+current_time+'\tFiltering of HD data with GATK - '+ID+'\n')
+        logi.write(' \n\n')
+
+
+    chromosome_list = list()
+    with open(chr_list,'r+') as chr_data:
+        for chr in chr_data.readlines():
+            chromosome_list.append(chr.strip())
+
+    for CHR in chromosome_list:
+        variants_input = out_dir+'/'+ID+'.SNPs_'+CHR+'.vcf.gz'
+        filter_output = out_dir+'/'+ID+'.HD_filt_'+CHR+'.vcf.gz'
+        select_output = out_dir+'/'+ID+'.HD_SNPs_'+CHR+'.vcf.gz'
+
+        filterCmd = 'gatk VariantFiltration -V '+variants_input+' -filter "QD < '+QD+'" --filter-name "QD" -filter "QUAL < '+QUAL+'" --filter-name "QUAL" -filter "FS > '+FS+'" --filter-name "FS" -O '+filter_output+''
+        subprocess.Popen(filterCmd,shell=True).wait()
+
+        selectCmd = 'gatk SelectVariants -V '+filter_output+' --exclude-filtered --select-type-to-include SNP -O '+select_output+''
+        subprocess.Popen(selectCmd,shell=True).wait()
+
+
+########## TO CONFIG:
+# QD < --
+# QUAL < --
+# FS < --
--- preprocessing_TMP.py
+++ preprocessing_TMP.py
@@ -14,6 +14,7 @@ parser.add_argument('-g', help="reference genome path or path to .tar.gz data ba
 parser.add_argument('-k', help="keep tmp directories", dest="keep", action='store_true')
 parser.add_argument('-l', help="pipeline log file", dest="log", required=False)
 parser.add_argument('-t', help="threads", dest="threads", required=True)
+parser.add_argument('-R', help="threads", dest="RERUN", action='store_true')
 args = parser.parse_args()
 
 in_f=args.input_txt
@@ -102,55 +103,81 @@ def in_out_preprocessing(path,in_f):
         output_files=''
         final_temp_dir="PPR_03-MappedToReference"
 
-        for line in lines:
-            ### Skip line if starts with # (comment line)
-            if not (line.startswith('#')):
+        if not args.RERUN:
+            if os.path.exists(in_dir):
+                rmCmd='rm -rf '+in_dir+''
+                subprocess.Popen(rmCmd,shell=True).wait()
+                os.makedirs(in_dir)
 
-                line = line.strip('\n').split(' ') # Create a list of each line
-                sample_name=line[0]
-                in_for=line[1]
-                in_rev=line[2]
+            for line in lines:
+                ### Skip line if starts with # (comment line)
+                if not (line.startswith('#')):
 
-                # Define output files based on input.txt
-                output_files+=path+'/'+final_temp_dir+'/'+sample_name+'_1.fastq.gz '
-                output_files+=path+'/'+final_temp_dir+'/'+sample_name+'_2.fastq.gz '
+                    line = line.strip('\n').split(' ') # Create a list of each line
+                    sample_name=line[0]
+                    in_for=line[1]
+                    in_rev=line[2]
 
-
-                # Define input file
-                in1=in_dir+'/'+sample_name+'_1.fastq.tmp'
-                # Check if input files already in desired dir
-                if os.path.isfile(in1):
-                    pass
-                else:
-                    #If the file is not in the working directory, transfer it
-                    if os.path.isfile(in_for) and not (os.path.isfile(in1)):
-                        if in_for.endswith('.gz'):
-                            read1Cmd = 'ln -s '+in_for+' '+in1+'.gz'
-                            subprocess.Popen(read1Cmd, shell=True).wait()
-                        else:
-                            read1Cmd = 'ln -s '+in_for+' '+in1+' && gzip -c '+in1+' > '+in1+'.gz'
-                            subprocess.Popen(read1Cmd, shell=True).wait()
+                    # Define output files based on input.txt
+                    output_files+=path+'/'+final_temp_dir+'/'+sample_name+'_1.fastq.gz '
+                    output_files+=path+'/'+final_temp_dir+'/'+sample_name+'_2.fastq.gz '
 
 
-                # Define input file
-                in2=in_dir+'/'+sample_name+'_2.fastq.tmp'
-                # Check if input files already in desired dir
-                if os.path.isfile(in2):
-                    pass
-                else:
-                    #If the file is not in the working directory, transfer it
-                    if os.path.isfile(in_rev) and not (os.path.isfile(in2)):
-                        if in_for.endswith('.gz'):
-                            read2Cmd = 'ln -s '+in_rev+' '+in2+'.gz'
-                            subprocess.Popen(read2Cmd, shell=True).wait()
-                        else:
-                            read2Cmd = 'ln -s '+in_rev+' '+in2+' && gzip -c '+in2+' > '+in2+'.gz'
-                            subprocess.Popen(read2Cmd, shell=True).wait()
+                    # Define input file
+                    in1=in_dir+'/'+sample_name+'_1.fastq.tmp'
+                    # Check if input files already in desired dir
+                    if os.path.isfile(in1):
+                        pass
+                    else:
+                        #If the file is not in the working directory, transfer it
+                        if os.path.isfile(in_for) and not (os.path.isfile(in1)):
+                            if in_for.endswith('.gz'):
+                                read1Cmd = 'ln -s '+in_for+' '+in1+'.gz'
+                                subprocess.Popen(read1Cmd, shell=True).wait()
+                            else:
+                                read1Cmd = 'ln -s '+in_for+' '+in1+' && gzip -c '+in1+' > '+in1+'.gz'
+                                subprocess.Popen(read1Cmd, shell=True).wait()
 
 
-                # Add stats and bam output files only once per sample
-                output_files+=(path+"/"+final_temp_dir+"/"+sample_name+".stats ")
-                output_files+=(path+"/"+final_temp_dir+"/"+sample_name+"_ref.bam ")
+                    # Define input file
+                    in2=in_dir+'/'+sample_name+'_2.fastq.tmp'
+                    # Check if input files already in desired dir
+                    if os.path.isfile(in2):
+                        pass
+                    else:
+                        #If the file is not in the working directory, transfer it
+                        if os.path.isfile(in_rev) and not (os.path.isfile(in2)):
+                            if in_for.endswith('.gz'):
+                                read2Cmd = 'ln -s '+in_rev+' '+in2+'.gz'
+                                subprocess.Popen(read2Cmd, shell=True).wait()
+                            else:
+                                read2Cmd = 'ln -s '+in_rev+' '+in2+' && gzip -c '+in2+' > '+in2+'.gz'
+                                subprocess.Popen(read2Cmd, shell=True).wait()
+
+
+                    # Add stats and bam output files only once per sample
+                    output_files+=(path+"/"+final_temp_dir+"/"+sample_name+".stats ")
+                    output_files+=(path+"/"+final_temp_dir+"/"+sample_name+"_ref.bam ")
+
+
+        if args.RERUN:
+            for line in lines:
+                ### Skip line if starts with # (comment line)
+                if not (line.startswith('#')):
+
+                    line = line.strip('\n').split(' ') # Create a list of each line
+                    sample_name=line[0]
+                    in_for=line[1]
+                    in_rev=line[2]
+
+                    # Define output files based on input.txt
+                    output_files+=path+'/'+final_temp_dir+'/'+sample_name+'_1.fastq.gz '
+                    output_files+=path+'/'+final_temp_dir+'/'+sample_name+'_2.fastq.gz '
+
+                    # Add stats and bam output files only once per sample
+                    output_files+=(path+"/"+final_temp_dir+"/"+sample_name+".stats ")
+                    output_files+=(path+"/"+final_temp_dir+"/"+sample_name+"_ref.bam ")
+
 
         return output_files
 
