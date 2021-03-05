e9af84125eb5a000e5bff087fbcb4c38a126914c
nuriaher <nuria.albala@sund.ku.dk>
2021-03-05 08:53:15 +0100

    upd

--- bin/holo-bin_quality.plot.R
+++ bin/holo-bin_quality.plot.R
@@ -18,7 +18,7 @@ out_path <- args$out_path
 
 
 
-# Run 
+# Run
 cov_data <- read.table(file=cov,header = T,quote = F,stringsAsFactors = F) # fields 1,3
 qual_data <- read.delim(file = qual,header = T, stringsAsFactors = F)
 qual_data <- as.data.frame(cbind(qual_data$Bin.Id,qual_data$Completeness,qual_data$Contamination))
@@ -27,10 +27,9 @@ colnames(qual_data) <- c("ID","Completeness","Contamination")
 # Generate df to plot: MAGid, completeness, contamination, avg coverage
 # Ensure total avg depth correspond to given contamination/completeness
 qual_data$avg_depth <- cov_data$totalAvgDepth[match(qual_data$ID,cov_data$MAGName)]
-  
-  
+
+
 qual <- ggplot()+geom_point(data=qual_data, aes(x=Completeness, y=Contamination, colour=avg_depth), size = 2)+
-scale_colour_gradient(low="#566643", high="#eb1c1c", "Total Average Depth") 
+scale_colour_gradient(low="#566643", high="#eb1c1c", "Total Average Depth")
 
-ggsave(plot = qual,filename = paste0(out_path,'/',ID,'/_quality.coverage_Plot.pdf'))
-
+ggsave(plot = qual,filename = paste0(out_path,'/',ID,'_quality.coverage_Plot.pdf'))
