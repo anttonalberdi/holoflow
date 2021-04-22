b086631ae91b43794dc75962b0722680f5909755
nuriaher <nuria.albala@sund.ku.dk>
2021-04-22 16:55:18 +0200

    upd

--- bin/holo-bin_quality.py
+++ bin/holo-bin_quality.py
@@ -31,36 +31,38 @@ threads=args.threads
 if not (os.path.exists(str(out_dir))):
     os.mkdir(str(out_dir))
 
-    # Write to log
-    current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
-    with open(str(log),'a+') as logi:
-        logi.write('\t\t'+current_time+'\tBin Quality step - '+ID+'\n')
-        logi.write('\n\n')
+# Write to log
+current_time = time.strftime("%m.%d.%y %H:%M", time.localtime())
+with open(str(log),'a+') as logi:
+    logi.write('\t\t'+current_time+'\tBin Quality step - '+ID+'\n')
+    logi.write('\n\n')
 
 
-    ## RUN
-
+## RUN
+if not os.path.isfile(out_dir+'/'+ID+'_binQuality.txt'):
     checkmCmd = 'module load anaconda2/4.0.0 hmmer/3.2.1 prodigal/2.6.3 pplacer/1.1.alpha17 && checkm lineage_wf -t '+threads+' -x fa '+bin_dir+' '+out_dir+' -f '+out_dir+'/'+ID+'_binQuality.txt'
     subprocess.Popen(checkmCmd,shell=True).wait()
 
-    rearraneoutCmd =' sed -i "s/--//g" '+out_dir+'/'+ID+'_binQuality.txt && sed -i "s/ \+ /\t/g" '+out_dir+'/'+ID+'_binQuality.txt'
-    subprocess.Popen(rearraneoutCmd,shell=True).wait()
+    rearrangeoutCmd =' sed -i "s/--//g" '+out_dir+'/'+ID+'_binQuality.txt && sed -i "s/ \+ /\t/g" '+out_dir+'/'+ID+'_binQuality.txt'
+    subprocess.Popen(rearrangeoutCmd,shell=True).wait()
 
-    # Plot quality - coverage
-    file = os.path.dirname(sys.argv[0])
-    curr_dir = os.path.abspath(file)
+# Plot quality - coverage
+file = os.path.dirname(sys.argv[0])
+curr_dir = os.path.abspath(file)
 
-    if os.path.isfile(out_dir+'/'+ID+'_binQuality.txt'):
-        plotCmd = 'module load tools gcc/5.4.0 intel/compiler/64/2018_update2 R/3.5.3-ICC-MKL && Rscript '+curr_dir+'/holo-bin_quality.plot.R -cov_data '+cov_file+' -qual_data '+out_dir+'/'+ID+'_binQuality.txt -ID '+ID+' -out_path '+out_dir+''
-        subprocess.Popen(plotCmd,shell=True).wait()
+if os.path.isfile(out_dir+'/'+ID+'_binQuality.txt'):
+    plotCmd = 'module load tools gcc/5.4.0 intel/compiler/64/2018_update2 R/3.5.3-ICC-MKL && Rscript '+curr_dir+'/holo-bin_quality.plot.R -cov_data '+str(cov_file)+' -qual_data '+out_dir+'/'+ID+'_binQuality.txt -ID '+ID+' -out_path '+out_dir+''
+    subprocess.Popen(plotCmd,shell=True).wait()
 
 
-    # Run summary table
-    input_drep_table = bin_dir+'/final_bins_Info.csv'
-    input_checkM_table = bin_dir+'/Widb.csv'
-    summary_table_tmp = out_dir+'/'+ID+'_binQuality_Info.tmp.csv'
-    summary_table = out_dir+'/'+ID+'_binQuality_Info.csv'
+# Run summary table
+input_drep_table = bin_dir+'/final_bins_Info.csv'
+input_checkM_table = bin_dir+'/Widb.csv'
+summary_table_tmp = out_dir+'/'+ID+'_binQuality_Info.tmp.csv'
+mag_table = out_dir+'/'+ID+'_binQuality_detail_Info.csv'
+summary_table = out_dir+'/'+ID+'_binQuality_general_Info.csv'
 
-    if os.path.isfile(out_dir+'/'+ID+'_binQuality.txt'):
-        summaryCmd = 'bash '+curr_dir+'/holo-bin_quality_table.sh '+input_drep_table+' '+input_checkM_table+' '+summary_table_tmp+' '+summary_table+''
-        subprocess.Popen(summaryCmd,shell=True).wait()
+
+if os.path.isfile(out_dir+'/'+ID+'_binQuality.txt'):
+    summaryCmd = 'bash '+curr_dir+'/holo-bin_quality_table.sh '+input_drep_table+' '+input_checkM_table+' '+summary_table_tmp+' '+mag_table+' '+summary_table+''
+    subprocess.Popen(summaryCmd,shell=True).wait()
--- bin/holo-bin_quality_table.sh
+++ bin/holo-bin_quality_table.sh
@@ -1,21 +1,27 @@
 in_data_drep=$1
 in_data_checkm=$2
 summary_table_tmp=$3
-summary_table=$4
-
+mag_table=$4
+summary_table=$5
 
 
 touch $summary_table_tmp
 while read line; do
-grep $line $in_data_drep | cut -d’,' -f1,2,3,5,6 >> $summary_table_tmp
-done < <(cut -d’,' -f1 $in_data_checkm)
-sort -t’,' -k2,2nr -k3,3n -k5,5nr $summary_table_tmp > $summary_table
+grep $line $in_data_drep | cut -d',' -f1,2,3,5,6 >> $summary_table_tmp
+done < <(cut -d',' -f1 $in_data_checkm)
+sort -t',' -k2,2nr -k3,3n -k5,5nr $summary_table_tmp > $mag_table
 rm $summary_table_tmp
 #All MAGs
-cat $summary_table | wc -l
-#Near complete
-awk -F ‘,’ ‘($2 > 98) && ($3 < 5) { print}’ $summary_table_tmp | wc -l
+echo '
+MAG SUMMARY
+        Total # MAGs' > $summary_table
+cat $mag_table | wc -l >> $summary_table
 #High quality
-awk -F ‘,’ ‘($2 > 90) && ($3 < 5) { print}’ $summary_table_tmp | wc -l
+echo '       High quality' >> $summary_table
+awk -F ',' '($2 > 90) && ($3 < 5) { print}' $mag_table | wc -l >> $summary_table
 #Good quality
-awk -F ‘,’ ‘($2 > 80) && ($3 < 10) { print}’ $summary_table_tmp | wc -l
+echo '  Good quality' >> $summary_table
+awk -F ',' '($2 > 80) && ($3 < 10) { print}' $mag_table | wc -l >> $summary_table
+#Near complete
+echo '  Nearly complete' >> $summary_table
+awk -F ',' '($2 > 98) && ($3 < 5) { print}' $mag_table | wc -l >> $summary_table
