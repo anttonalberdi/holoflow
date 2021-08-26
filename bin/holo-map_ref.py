d2426264d00eeee6d7279204383202a39f37eb2a
EisenRa <raph.eisenhofer@gmail.com>
2021-08-26 11:56:04 +0930

    add -S

--- bin/holo-map_ref.py
+++ bin/holo-map_ref.py
@@ -48,7 +48,7 @@ mapCmd = 'module load tools samtools/1.11 bowtie2/2.4.2 pigz/2.3.4 \
           --rg-id "@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:'+ID+'" \
           -x '+ref_gen+' \
           -1 '+read1+' \
-          -2 '+read2+' | samtools view -@ '+threads+' -T '+ref_gen+' -b - > '+all_bam+''
+          -2 '+read2+' | samtools view -@ '+threads+' -T '+ref_gen+' -bS - > '+all_bam+''
 subprocess.check_call(mapCmd, shell=True,executable="/bin/bash")
 #     else:
 #         mapCmd = 'module load tools samtools/1.11 bwa/0.7.15 && bwa mem -t '+t+' -k 19 -w '+w+' -d '+d+' -A '+A+' -B '+B+' -O '+O+' -E '+E+' -L '+L+' -R "@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:'+ID+'" '+ref_gen+' <(gunzip -c '+read1+') <(gunzip -c '+read2+') | samtools view -T '+ref_gen+' -b - > '+all_bam+''
